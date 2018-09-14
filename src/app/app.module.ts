ddbce687e11d5bb3e5771554027212e4d7262e48
Mejía Bedoya <melinamejia95@gmail.com>
2018-09-14 11:38:15 -0500

    Ejemplos pruebas intermedias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MedicosService } from './intermedio/espias/medicos.service';
+import { MedicosComponent } from './intermedio/espias/medicos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MedicosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
